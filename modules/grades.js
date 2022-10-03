d3a573d38cf553bb458945bd3ca84d5411625926
Dorian Eydoux <loaves_pickle0@icloud.com>
2022-10-03 23:11:54 +0200

    Add colored emoji based on grade ratio

--- modules/grades.js
+++ modules/grades.js
@@ -4,6 +4,7 @@ const cache = existsSync('./cache/grades.json') ? require('../cache/grades.json'
 
 const { bold } = require('../utils/formatters')
 const subjects = require('../utils/subjects.json')
+const emojis = ['⚫️', '🟤', '🔴', '🟠', '🟡', '🟢', '🔵', '⚪️']
 
 module.exports = async ed => {
     const grades = (await ed.getGrades())
@@ -12,7 +13,8 @@ module.exports = async ed => {
     for (const grade of grades) {
         console.info(`New grade #${grade._raw.id}`)
         cache.push(grade._raw.id)
-        const message = `🧮 ${bold('New grade')}\n${grade.name}\n\n✨ ${bold(grade.value)} / ${grade.outOf}\n${grade.weight !== 1 ? `🎚 Coefficient ${bold(grade.weight)}\n` : ''}📊 Averange ${bold(grade.classAvg)}\n🧑🏻‍🏫 ${subjects[grade.subjectCode]}\n🗓 ${grade.date.toLocaleDateString('fr')}`
+        const emoji = emojis[Math.round(grade.value / grade.outOf * (emojis.length - 1))]
+        const message = `🧮 ${bold('New grade')}\n${grade.name}\n\n${emoji} ${bold(grade.value)} / ${grade.outOf}\n${grade.weight !== 1 ? `🎚 Coefficient ${bold(grade.weight)}\n` : ''}📊 Averange ${bold(grade.classAvg)}\n🧑🏻‍🏫 ${subjects[grade.subjectCode]}\n🗓 ${grade.date.toLocaleDateString('fr')}`
         await telegram(message)
     }
 

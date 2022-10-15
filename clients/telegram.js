83d046ab5fd5e2ce2595674f9ff98fd93737037d
Dorian Eydoux <loaves_pickle0@icloud.com>
2022-10-15 18:45:53 +0200

    Add console output mode

--- clients/telegram.js
+++ clients/telegram.js
@@ -1,4 +1,7 @@
 const { Telegraf } = require('telegraf')
 const client = new Telegraf(process.env.TELEGRAM_TOKEN)
 
-module.exports = text => client.telegram.sendMessage(process.env.TELEGRAM_CHAT, text, { parse_mode: 'HTML' })
+
+module.exports = text => process.argv.includes('--output=console')
+    ? console.debug(text)
+    : client.telegram.sendMessage(process.env.TELEGRAM_CHAT, text, { parse_mode: 'HTML' })
